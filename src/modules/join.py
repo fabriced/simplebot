0c91ef4eda93d9e5b1af3a160d1c7899dd48a830
Fabrice Decroix <fabrice.decroix@gmail.com>
2008-12-24 01:50:31 +0100

    fix an error

--- src/modules/join.py
+++ src/modules/join.py
@@ -3,7 +3,7 @@ from lib.exception import *
 class join(object):
   def __init__(self, main, mask, channel, message):
     if message.split()[1:]:
-      if main.is_admin(host):
+      if main.is_admin(mask):
         main.s.send("JOIN %s\n" % message.split()[1])
       else:
         raise NotAdminError
