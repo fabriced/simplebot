f0d89abf9d2db849671a1d73d66c5bbe8953a2af
Fabrice Decroix <fabrice.decroix@gmail.com>
2008-12-28 22:19:33 +0100

    move format_nick function to the scan module

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,11 +1,5 @@
 #*-* Coding: UTF-8 *-*
 
-
-
 def get_nick(host):
   return host.split("!")[0]
 
-def format_nick(nick):
-  """ format Quake 3 players nicknames to remove color infos """
-  p = re.compile( '\^.')
-  return p.sub('',nick)
