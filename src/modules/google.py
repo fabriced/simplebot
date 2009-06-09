3f11d8083d0112a03aac1c74d1716ae1d2bbbf57
Fabrice Decroix <fabrice.decroix@gmail.com>
2009-06-10 00:12:56 +0200

    fix indent and bad nammed class in admin.py

--- src/modules/google.py
+++ src/modules/google.py
@@ -5,6 +5,6 @@ from command import command
 class google(command):
   def do(self):
     if len(self.message.split(None, 1))>1:
-    	self.main.say('http://www.google.com/search?q=%s' % self.message.split(None, 1)[1], self.channel)
+      self.main.say('http://www.google.com/search?q=%s' % self.message.split(None, 1)[1], self.channel)
     else:
-    	self.main.say('http://www.google.com', self.channel)
+      self.main.say('http://www.google.com', self.channel)
