890327ce9cd9b001e02951d5864383f7a3a4f020
Fabrice Decroix <fabrice.decroix@gmail.com>
2009-06-10 00:08:13 +0200

    fix indent

--- src/modules/spam.py
+++ src/modules/spam.py
@@ -9,10 +9,10 @@ class spam(command):
 
   @admin_required
   def do(self):
-  	if len(self.message.split(None, 1))>1:
-  		phrase = self.message.split(None, 1)[1]
-  		for chan in CHANNELS:
-  			if chan != self.channel:
-				self.main.say('-spam- %s' % phrase, chan)
-	else:
-		self.main.say('Spam quoi, gros niais ?', get_nick(self.mask))
+    if len(self.message.split(None, 1))>1:
+      phrase = self.message.split(None, 1)[1]
+      for chan in CHANNELS:
+        if chan != self.channel:
+        self.main.say('-spam- %s' % phrase, chan)
+  else:
+    self.main.say('Spam quoi, gros niais ?', get_nick(self.mask))
--- src/modules/wiki.py
+++ src/modules/wiki.py
@@ -5,6 +5,6 @@ from command import command
 class wiki(command):
   def do(self):
     if len(self.message.split(None, 1))>1:
-    	self.main.say('http://fr.wikipedia.org/wiki/%s' % self.message.split(None, 1)[1], self.channel)
+      self.main.say('http://fr.wikipedia.org/wiki/%s' % self.message.split(None, 1)[1], self.channel)
     else:
-    	self.main.say('http://fr.wikipedia.org', self.channel)
+      self.main.say('http://fr.wikipedia.org', self.channel)
