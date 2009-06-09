850222beddad2e9fb96be6bf61242355386f9b62
skuizh <skuizh@skuizh-debian.(none)>
2009-06-09 23:57:15 +0200

    update localisation on main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import locale
 from simplebot import *
 
 
 if __name__ == "__main__":
+  locale.setlocale(locale.LC_ALL, 'fr_FR.utf-8')
   bot = fafaIrcBot()
   bot.main_loop()
