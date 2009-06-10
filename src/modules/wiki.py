e2b934db54355a86fff8e62d0bd3763f62c1e6ef
Fabrice Decroix <fabrice.decroix@gmail.com>
2009-06-10 09:09:01 +0200

    fix wiki for the last update in command class

--- src/modules/wiki.py
+++ src/modules/wiki.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 
+#TODO check the url for 404
+
 from command import command
 
 class wiki(command):
   def do(self):
-    if len(self.message.split(None, 1))>1:
-      self.main.say('http://fr.wikipedia.org/wiki/%s' % self.message.split(None, 1)[1], self.channel)
+    if self.message:
+      self.main.say('http://fr.wikipedia.org/wiki/%s' % self.message.split()[0], self.channel)
     else:
       self.main.say('http://fr.wikipedia.org', self.channel)
